c7452175a4938606a099ad857b501572e4b1020d
Kirihara Shu <kirihara.shu-p@pfu.fujitsu.com>
2020-03-27 08:35:29 +0900

    memo update

--- @Documents/カタログ情報いろいろ.sql
+++ @Documents/カタログ情報いろいろ.sql
@@ -1,3 +1,20 @@
+/*
+[TABLE]
+  schema_name
+  table_name
+  comment
+
+[COLUMN]
+
+
+
+
+
+
+
+
+*/
+
 -- =======================================================================================
 -- PostgreSQL
 -- =======================================================================================
@@ -67,8 +84,41 @@ SELECT table_schema
 -- =======================================================================================
 -- SQL Server
 -- =======================================================================================
+SELECT tbl.name AS TableName
+     , scm.name AS SchemaName
+     , eps.value AS Comment
+  FROM sys.tables AS tbl
+        LEFT JOIN sys.schemas AS scm
+               ON tbl.schema_id = scm.schema_id
+        LEFT JOIN sys.extended_properties eps
+               ON tbl.object_id = eps.major_id
+              AND eps.minor_id = 0
+ WHERE tbl.type = 'U'
+-- Viewは sys.tablesの代わりに sys.views
 
-
-
-
+SELECT cif.table_catalog AS [Database]
+     , cif.table_schema AS [Owner]
+     , cif.table_name AS TableName
+     , cif.column_name AS ColumnName
+     , cif.ordinal_position AS OrdinalPosition
+     , cif.column_default AS DefaultSetting
+     , cif.is_nullable AS IsNullable
+     , cif.data_type AS DataType
+     , cif.character_maximum_length AS [MaxLength]
+     , cif.datetime_precision AS DatePrecision
+     , COLUMNPROPERTY(object_id('[' + table_schema + '].[' + table_name + ']'), column_name, 'IsIdentity') AS IsIdentity
+     , COLUMNPROPERTY(object_id('[' + table_schema + '].[' + table_name + ']'), column_name, 'IsComputed') AS IsComputed
+     , exp.value AS Commnet
+  FROM information_schema.columns AS cif
+        LEFT JOIN sys.tables AS tbl
+               ON cif.table_name = tbl.name
+        LEFT JOIN sys.columns AS col
+               ON tbl.object_id = col.object_id
+              AND cif.column_name = col.name
+        LEFT JOIN sys.extended_properties AS exp
+               ON col.object_id = exp.major_id
+              AND col.column_id = exp.minor_id
+ WHERE cif.table_name = @tablename
+   AND cif.table_schema = @schemaname
+ ORDER BY cif.ordinal_position ASC;
 
